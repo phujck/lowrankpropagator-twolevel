6d655a263b0026fb6d91528461b326cb0905f922
phujck <gmccaul@tulane.edu>
2020-04-23 13:40:19 -0500

    added spinchain example. started testing timings for the speed of the low rank propagation. V. encouraging!

---
+++ spinchain.py
@@ -0,0 +1,287 @@
+from qutip import *
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import time
+
+params = {
+    'axes.labelsize': 30,
+    # 'legend.fontsize': 28,
+    'legend.fontsize': 23,
+    'xtick.labelsize': 22,
+    'ytick.labelsize': 22,
+    'figure.figsize': [2 * 3.375, 2 * 3.375],
+    'text.usetex': True,
+    'figure.figsize': (16, 12)
+}
+
+plt.rcParams.update(params)
+
+
+def integrate_setup(N, h, Jx, Jy, Jz, gamma, leads):
+    si = qeye(2)
+    sx = sigmax()
+    sy = sigmay()
+    sz = sigmaz()
+    sp = sigmap()
+    sm = sigmam()
+
+    sx_list = []
+    sy_list = []
+    sz_list = []
+
+    for n in range(N):
+        op_list = []
+        for m in range(N):
+            op_list.append(si)
+
+        op_list[n] = sx
+        sx_list.append(tensor(op_list))
+
+        op_list[n] = sy
+        sy_list.append(tensor(op_list))
+
+        op_list[n] = sz
+        sz_list.append(tensor(op_list))
+
+    # construct the hamiltonian
+    H = 0
+
+    # energy splitting terms
+    for n in range(N):
+        H += - 0.5 * h[n] * sz_list[n]
+
+    # interaction terms
+    for n in range(N - 1):
+        H += - 0.5 * Jx[n] * sx_list[n] * sx_list[n + 1]
+        H += - 0.5 * Jy[n] * sy_list[n] * sy_list[n + 1]
+        H += - 0.5 * Jz[n] * sz_list[n] * sz_list[n + 1]
+
+    # collapse operators
+    c_op_list = []
+
+    # spin dephasing
+    for n in range(N):
+        if gamma[n] > 0.0:
+            c_op_list.append(np.sqrt(gamma[n]) * sz_list[n])
+
+    # adding coupling to bath:
+    if leads[0] > 0:
+        mu = leads[1]
+        gamma_l = leads[0]
+        op_list = []
+        for m in range(N):
+            op_list.append(si)
+        zero_minus = op_list.copy()
+        zero_plus = op_list.copy()
+        N_minus = op_list.copy()
+        N_plus = op_list.copy()
+        zero_minus[0] = sm * np.sqrt(gamma_l * (1 + mu))
+        zero_plus[0] = sp * np.sqrt(gamma_l * (1 - mu))
+        N_minus[-1] = sm * np.sqrt(gamma_l * (1 - mu))
+        N_plus[-1] = sp * np.sqrt(gamma_l * (1 + mu))
+        c_op_list.append(tensor(zero_minus))
+        c_op_list.append(tensor(zero_plus))
+        # c_op_list.append(tensor(N_minus))
+        # c_op_list.append(tensor(N_plus))
+    return H, sz_list, sx_list, c_op_list
+
+
+def qutip_solver(H, psi0, tlist, c_op_list, sz_list, solver):
+    # evolve and calculate expectation values
+    if solver == "me":
+        result = mesolve(H, psi0, tlist, c_op_list, sz_list, progress_bar=True)
+    elif solver == "mc":
+        ntraj = 250
+        result = mcsolve(H, psi0, tlist, c_op_list, sz_list, ntraj, progress_bar=True)
+
+    return result.expect
+
+
+def unitaries(H, c_op_list, dt):
+    K = len(c_op_list)
+    prefactor = np.sqrt(1 / (2 * K))
+    U_ops = []
+    for op in c_op_list:
+        exponent = 1j * H * dt
+        if op.isherm == False:
+            exponent += (dt * K / 2) * (op * op - op.dag() * op)
+        extra = 1j * np.sqrt(K * dt) * op
+        P1 = exponent + extra
+        P2 = exponent - extra
+        U_prop=prefactor*(P1.expm())
+        V_prop=prefactor*(P2.expm())
+        U_ops.append(U_prop)
+        U_ops.append(V_prop)
+    return U_ops
+
+def overlap(psi_list):
+    matrix = np.zeros((len(psi_list), len(psi_list)), dtype=np.complex_)
+    for i in range(len(psi_list)):
+        for j in range(i, len(psi_list)):
+            # overlap=psi_list[i].dag()*psi_list[j]
+            # print('method 1')
+            # print(overlap.data)
+            # print('method 2')
+            o2 = psi_list[i].overlap(psi_list[j])
+            # print(o2)
+            matrix[i, j] = o2
+            if j > i:
+                matrix[j, i] = np.conj(o2)
+    # print(matrix)
+    return matrix
+
+
+def orthogonalise(psi_list, U, zeroobj, rank):
+    g_list = []
+    U = U.T
+    if rank > len(psi_list):
+        rank = len(psi_list)
+    for i in range(rank):
+        norm = 0
+        g = zeroobj
+        for j in range(len(psi_list)):
+            # print(U[i,j])
+            # print(psi_list[j].dims)
+            g_unit = U[i, j] * psi_list[j]
+            g += g_unit
+        g_list.append(g)
+    return g_list
+
+def one_step(psis,U_ops,zeropsi,rank):
+    new_psis=[]
+    for psi in psis:
+        for U in U_ops:
+            newpsi=U*psi
+            new_psis.append(newpsi)
+    if len(new_psis) > rank:
+        mat = overlap(new_psis)
+        w, v = np.linalg.eig(mat)
+        idx = w.argsort()[::-1]
+        w = w[idx]
+        v = v[:, idx]
+        new_psis = orthogonalise(new_psis, v, zeropsi, rank)
+    return new_psis
+
+# set up the calculation
+#
+solver = "me"  # use the ode solver
+# solver = "mc"   # use the monte-carlo solver
+
+N = 12  # number of spins
+
+# array of spin energy splittings and coupling strengths. here we use
+# uniform parameters, but in general we don't have too
+h = 0 * 2 * np.pi * np.ones(N)
+Jz = 0.1 * 2 * np.pi * np.ones(N)
+Jx = 0.1 * 2 * np.pi * np.ones(N)
+# Jx = 0 * 2 * np.pi * np.ones(N)
+Jy = 0.1 * 2 * np.pi * np.ones(N)
+# dephasing rate
+# gamma = 0.01 * np.ones(N)
+dephase = 0
+gamma = dephase * np.ones(N)
+
+# leads coupling
+leads = np.zeros(2)
+# bath coupling
+bath_couple = 0.1
+driving = 0.2
+leads[0] = bath_couple
+# driving
+leads[1] = driving
+# driving
+# intial state, first spin in state |1>, the rest in state |0>
+psi_list = []
+zero_list=[]
+zero_ket=Qobj([[0], [0]])
+# psi_list.append(basis(2,1))
+# for n in range(N-1):
+#     psi_list.append(basis(2,0))
+
+start=time.time()
+for n in range(N):
+    psi_list.append((basis(2, 0) + basis(2, 1)).unit())
+    zero_list.append(zero_ket)
+
+# for n in range(N):
+#     if n %2 ==0:
+#         psi_list.append(basis(2,0))
+#     else:
+#         psi_list.append(basis(2,1))
+psi0 = tensor(psi_list)
+zeropsi=tensor(zero_list)
+# print(zeropsi)
+steps = 2 * 400
+tlist, deltat = np.linspace(0, 200, steps, retstep=True)
+rank=8
+H, sz_list, sx_list, c_op_list = integrate_setup(N, h, Jx, Jy, Jz, gamma, leads)
+U_ops= unitaries(H,c_op_list,deltat)
+end=time.time()
+print('operators built! Time taken %.3f seconds' % (end-start))
+psis=[psi0]
+
+# visualise sparsity
+loop_start=time.time()
+for j in range(0,50):
+    psis=one_step(psis,U_ops,zeropsi,rank)
+    newend = time.time()
+    start=time.time()
+    norms=[psi.norm()**2 for psi in psis]
+    norm=np.sum(norms)
+    psi_list = [psi/np.sqrt(norm) for psi in psis]
+    end=time.time()
+    print('Normalisation done. Time taken %.5f seconds' % (end-start))
+loop_end=time.time()
+# psi_list = [psi for psi in psis]
+print('50 steps done. Time taken %.5f seconds' % (loop_end - loop_start))
+
+mat = overlap(psi_list)
+print(np.sum(np.diag(mat)))
+fig, ax = plt.subplots()
+ax.matshow(mat.real, cmap='seismic')
+
+for (i, j), z in np.ndenumerate(mat.real):
+    ax.text(j, i, '{:0.5f}'.format(z), ha='center', va='center')
+
+
+# plt.spy(mat,markersize=2,precision=10**(-6))
+
+plt.show()
+
+print('one step calculated! Time taken %.3f seconds' % (newend - end))
+
+# print(psis)
+
+
+
+# for j in range(len(c_op_list)):
+#     print(c_op_list[j].isherm)
+# sz_expt = qutip_solver(H, psi0, tlist, c_op_list, sz_list + sx_list, solver)
+#
+# fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 6))
+# sz_tot = np.zeros(len(sz_expt[0]))
+# for n in range(N):
+#     sz_tot += sz_expt[n]
+#
+# for n in range(N):
+#     ax1.plot(tlist, np.real(sz_expt[n]), label='$\\langle\\sigma_z^{(%d)}\\rangle$' % n)
+#     ax2.plot(tlist, np.real(sz_expt[N + n]), label='$\\langle\\sigma_x^{(%d)}\\rangle$' % n)
+# ax1.legend(loc=0)
+# ax2.set_xlabel('Time [ns]')
+# ax1.set_ylabel('$\\langle\sigma_z\\rangle$')
+# ax2.set_ylabel('$\\langle\sigma_x\\rangle$')
+# ax2.legend()
+#
+# ax1.set_title('Dynamics of a Heisenberg spin chain')
+# plt.show()
+#
+# plt.plot(tlist, sz_tot)
+# plt.xlabel('Time [ns]')
+# plt.ylabel('$\\langle\sigma^{\\rm tot}_z\\rangle$')
+# plt.title('Dynamics of a Heisenberg spin chain')
+# plt.show()
+#
+# final_sz = [sz_expt[n][-1] for n in range(N)]
+# plt.plot(range(N), final_sz)
+# plt.show()
--- twolevel.py
+++ twolevel.py
@@ -2,6 +2,7 @@ from qutip import *
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+
 params = {
     'axes.labelsize': 30,
     # 'legend.fontsize': 28,
@@ -14,25 +15,28 @@ params = {
 }
 
 plt.rcParams.update(params)
-default_times=np.linspace(-50,50,10000)
-def evolution_LZ(delta=1,velocity=1,gamma1=0,gamma2=0,rho=fock_dm(2,0),times=default_times):
+default_times = np.linspace(-50, 50, 10000)
+
+
+def evolution_LZ(delta=1, velocity=1, gamma1=0, gamma2=0, rho=fock_dm(2, 0), times=default_times):
     sx = sigmax()
     sy = sigmay()
     sz = sigmaz()
     sp = sigmap()
     sm = sigmam()
 
-    c_op_list=[]
-    if gamma1 >0.0:
-        c_op_list.append(np.sqrt(gamma1)*sz)
+    c_op_list = []
+    if gamma1 > 0.0:
+        c_op_list.append(np.sqrt(gamma1) * sz)
     if gamma2 > 0.0:
-        c_op_list.append(np.sqrt(gamma2)*sm)
+        c_op_list.append(np.sqrt(gamma2) * sm)
 
-    H0=delta*sx
-    H=[H0,[velocity*sz,'t']]
-    result=mesolve(H,rho_0,times, c_op_list,e_ops=[sx,sz],progress_bar=True)
+    H0 = delta * sx
+    H = [H0, [velocity * sz, 't']]
+    result = mesolve(H, rho_0, times, c_op_list, e_ops=[sx, sz], progress_bar=True)
 
     return result.expect
+
 
 # velocity=0.005
 # delta=1
@@ -49,23 +53,25 @@ def evolution_LZ(delta=1,velocity=1,gamma1=0,gamma2=0,rho=fock_dm(2,0),times=def
 # plt.plot(times,np.ones(len(times))*szlimit)
 # plt.show()
 
-def evolution(delta=1,epsilon=1,gamma1=0,gamma2=0,rho=fock_dm(2,0),times=default_times):
+def evolution(delta=1, epsilon=1, gamma1=0, gamma2=0, rho=fock_dm(2, 0), times=default_times):
     sx = sigmax()
     sy = sigmay()
     sz = sigmaz()
     sp = sigmap()
     sm = sigmam()
 
-    c_op_list=[]
-    if gamma1 >0.0:
-        c_op_list.append(np.sqrt(gamma1)*sz)
+    c_op_list = []
+    if gamma1 > 0.0:
+        c_op_list.append(np.sqrt(gamma1) * sz)
     if gamma2 > 0.0:
-        c_op_list.append(np.sqrt(gamma2)*sm)
+        c_op_list.append(np.sqrt(gamma2) * sm)
 
-    H=delta*sx+epsilon*sz
-    result=mesolve(H,rho_0,times, c_op_list,e_ops=[sx,sz],progress_bar=True)
+    H = delta * sx + epsilon * sz
+    result = mesolve(H, rho_0, times, c_op_list, e_ops=[sx, sz], progress_bar=True)
     print(result)
     return result.expect
+
+
 # epsilon=1
 # delta=1
 # gamma1=1
@@ -80,75 +86,79 @@ def evolution(delta=1,epsilon=1,gamma1=0,gamma2=0,rho=fock_dm(2,0),times=default
 # # plt.plot(times,np.ones(len(times))*szlimit)
 # plt.show()
 
-def U_prop_sz(psi,epsilon=1,delta=1,gamma=0,K=1,dt=10**-5):
+def U_prop_sz(psi, epsilon=1, delta=1, gamma=0, K=1, dt=10 ** -5):
     sx = sigmax()
     sz = sigmaz()
 
-    H0=epsilon*sz+delta*sx
-    HI=-1j*sz*np.sqrt(K*gamma*dt)
-    H=1j*H0*dt+HI
-    prop_U=H.expm()
+    H0 = epsilon * sz + delta * sx
+    HI = -1j * sz * np.sqrt(K * gamma * dt)
+    H = 1j * H0 * dt + HI
+    prop_U = H.expm()
     result = prop_U * psi
     return result
-def V_prop_sz(psi,epsilon=1,delta=1,gamma=0,K=1,dt=10**-5):
+
+
+def V_prop_sz(psi, epsilon=1, delta=1, gamma=0, K=1, dt=10 ** -5):
     sx = sigmax()
     sz = sigmaz()
 
-    H0=epsilon*sz+delta*sx
-    HI=+1j*sz*np.sqrt(K*gamma*dt)
-    H=1j*H0*dt+HI
-    prop_V=H.expm()
-    result=prop_V*psi
-    # print('propagator result')
-    # print(result)
-    return result
-
-def U_prop_sm(psi,epsilon=1,delta=1,gamma=0,K=1,dt=10**-5):
-    sx = sigmax()
-    sz = sigmaz()
-    sm = sigmam()
-    H0=epsilon*sz+delta*sx -1j*gamma*(K/2)*(sm*sm-sm.dag()*sm)
-    HI=-1j*sm*np.sqrt(K*gamma*dt)
-    H=1j*H0*dt+HI
-    prop_U=H.expm()
-    result = prop_U * psi
-    return result
-def V_prop_sm(psi,epsilon=1,delta=1,gamma=0,K=1,dt=10**-5):
-    sx = sigmax()
-    sz = sigmaz()
-    sm = sigmam()
-    H0=epsilon*sz+delta*sx-1j*gamma*(K/2)*(sm*sm-sm.dag()*sm)
-
-    HI=+1j*sm*np.sqrt(K*gamma*dt)
-    H=1j*H0*dt+HI
-    prop_V=H.expm()
-    result=prop_V*psi
+    H0 = epsilon * sz + delta * sx
+    HI = +1j * sz * np.sqrt(K * gamma * dt)
+    H = 1j * H0 * dt + HI
+    prop_V = H.expm()
+    result = prop_V * psi
     # print('propagator result')
     # print(result)
     return result
 
 
-def one_step(psi_list,epsilon=1,delta=1,gamma=0,dt=10**-5):
-    psis=[]
+def U_prop_sm(psi, epsilon=1, delta=1, gamma=0, K=1, dt=10 ** -5):
+    sx = sigmax()
+    sz = sigmaz()
+    sm = sigmam()
+    H0 = epsilon * sz + delta * sx - 1j * gamma * (K / 2) * (sm * sm - sm.dag() * sm)
+    HI = -1j * sm * np.sqrt(K * gamma * dt)
+    H = 1j * H0 * dt + HI
+    prop_U = H.expm()
+    result = prop_U * psi
+    return result
+
+
+def V_prop_sm(psi, epsilon=1, delta=1, gamma=0, K=1, dt=10 ** -5):
+    sx = sigmax()
+    sz = sigmaz()
+    sm = sigmam()
+    H0 = epsilon * sz + delta * sx - 1j * gamma * (K / 2) * (sm * sm - sm.dag() * sm)
+
+    HI = +1j * sm * np.sqrt(K * gamma * dt)
+    H = 1j * H0 * dt + HI
+    prop_V = H.expm()
+    result = prop_V * psi
+    # print('propagator result')
+    # print(result)
+    return result
+
+
+def one_step(psi_list, epsilon=1, delta=1, gamma=0, dt=10 ** -5):
+    psis = []
     for psi in psi_list:
-        psis.append(U_prop_sz(psi,epsilon,delta,gamma,1,dt))
-        psis.append(V_prop_sz(psi,epsilon,delta,gamma,1,dt))
+        psis.append(U_prop_sz(psi, epsilon, delta, gamma, 1, dt))
+        psis.append(V_prop_sz(psi, epsilon, delta, gamma, 1, dt))
     # print('last psi')
     return psis
 
 
-
-def one_step_multiple(psi_list,epsilon=1,delta=1,gamma1=0,gamma2=0,dt=10**-5):
-    psis=[]
-    K=1
-    if gamma1 >0 and gamma2 > 0:
-       K=2
-    prefactor=np.sqrt(1/(2*K))
+def one_step_multiple(psi_list, epsilon=1, delta=1, gamma1=0, gamma2=0, dt=10 ** -5):
+    psis = []
+    K = 1
+    if gamma1 > 0 and gamma2 > 0:
+        K = 2
+    prefactor = np.sqrt(1 / (2 * K))
     for psi in psi_list:
-        if gamma1 >0:
-            psis.append(prefactor*U_prop_sz(psi,epsilon,delta,gamma1,K,dt))
-            psis.append(prefactor*V_prop_sz(psi,epsilon,delta,gamma1,K,dt))
-        if gamma2>0:
+        if gamma1 > 0:
+            psis.append(prefactor * U_prop_sz(psi, epsilon, delta, gamma1, K, dt))
+            psis.append(prefactor * V_prop_sz(psi, epsilon, delta, gamma1, K, dt))
+        if gamma2 > 0:
             psis.append(prefactor * U_prop_sm(psi, epsilon, delta, gamma2, K, dt))
             psis.append(prefactor * V_prop_sm(psi, epsilon, delta, gamma2, K, dt))
         else:
@@ -157,51 +167,53 @@ def one_step_multiple(psi_list,epsilon=1,delta=1,gamma1=0,gamma2=0,dt=10**-5):
     # print('last psi')
     return psis
 
+
 def overlap(psi_list):
-    matrix=np.zeros((len(psi_list),len(psi_list)),dtype=np.complex_)
+    matrix = np.zeros((len(psi_list), len(psi_list)), dtype=np.complex_)
     for i in range(len(psi_list)):
-        for j in range(i,len(psi_list)):
+        for j in range(i, len(psi_list)):
             # overlap=psi_list[i].dag()*psi_list[j]
             # print('method 1')
             # print(overlap.data)
             # print('method 2')
-            o2=psi_list[i].overlap(psi_list[j])
+            o2 = psi_list[i].overlap(psi_list[j])
             # print(o2)
-            matrix[i,j]= o2
-            if j >i:
-                matrix[j,i]=np.conj(o2)
+            matrix[i, j] = o2
+            if j > i:
+                matrix[j, i] = np.conj(o2)
     # print(matrix)
     return matrix
 
-def orthogonalise(psi_list,U,rank):
-    g_list=[]
-    U=U.T
+
+def orthogonalise(psi_list, U, rank):
+    g_list = []
+    U = U.T
     if rank > len(psi_list):
-        rank=len(psi_list)
+        rank = len(psi_list)
     for i in range(rank):
         norm = 0
-        g=Qobj([[0],[0]])
+        g = Qobj([[0], [0]])
         for j in range(len(psi_list)):
             # print(U[i,j])
             # print(psi_list[j].dims)
-            g_unit=U[i,j]*psi_list[j]
-            g+=g_unit
+            g_unit = U[i, j] * psi_list[j]
+            g += g_unit
         g_list.append(g)
 
     return g_list
 
 
 def rho(psi_list):
-    zeroket=Qobj([[0],[0]])
+    zeroket = Qobj([[0], [0]])
     # print(zeroket)
-    rho=ket2dm(zeroket)
+    rho = ket2dm(zeroket)
     # print(rho)
     for psi in psi_list:
         # print('psi is')
         # print(psi.unit())
         # rhoj=ket2dm(psi)
         # print(rhoj)
-        rho+=ket2dm(psi)
+        rho += ket2dm(psi)
         # rho=rho.unit()
     # print('rho is')
     # print(rho)
@@ -209,42 +221,42 @@ def rho(psi_list):
     # print(rho.type)
     return rho
 
-dt=10**-5
-epsilon=1
-delta=1
-gamma=0.1
-gamma1=gamma
-gamma2=0.2
-rho_0=fock_dm(2,0)
-psi_0=basis(2,0)
-psis=one_step([psi_0],epsilon,delta,gamma,dt)
+
+dt = 10 ** -5
+epsilon = 1
+delta = 1
+gamma = 0.1
+gamma1 = gamma
+gamma2 = 0.2
+rho_0 = fock_dm(2, 0)
+psi_0 = basis(2, 0)
+psis = one_step([psi_0], epsilon, delta, gamma, dt)
 print(psis[0])
 print(psis[1])
 
-N=1000
-times,dt=np.linspace(0,25,N,retstep=True)
+N = 1000
+times, dt = np.linspace(0, 25, N, retstep=True)
 print("timestep")
 print(dt)
 
-
 sz = sigmaz()
-sx=sigmax()
+sx = sigmax()
 
 # z_track.append((sz * rho(psi_list)).tr())
-expect=evolution(delta,epsilon,gamma1,gamma2,rho_0,times)
-expect_unaltered=evolution(delta,epsilon,0,0,rho_0,times)
+expect = evolution(delta, epsilon, gamma1, gamma2, rho_0, times)
+expect_unaltered = evolution(delta, epsilon, 0, 0, rho_0, times)
 
-for rank in [1,2]:
+for rank in [1, 2]:
     psi_list = [psi_0]
     z_track = []
-    x_track=[]
-    for k in tqdm(range(0,N)):
-        z_track.append((sz*rho(psi_list)).tr())
-        x_track.append((sx*rho(psi_list)).tr())
+    x_track = []
+    for k in tqdm(range(0, N)):
+        z_track.append((sz * rho(psi_list)).tr())
+        x_track.append((sx * rho(psi_list)).tr())
         # psi_list=one_step(psi_list,epsilon,delta,gamma,dt)
-        psi_list=one_step_multiple(psi_list,epsilon,delta,gamma1,gamma2,dt)
+        psi_list = one_step_multiple(psi_list, epsilon, delta, gamma1, gamma2, dt)
 
-        if len(psi_list)>rank:
+        if len(psi_list) > rank:
             mat = overlap(psi_list)
             w, v = np.linalg.eig(mat)
             idx = w.argsort()[::-1]
@@ -262,27 +274,21 @@ for rank in [1,2]:
     # for (i, j), z in np.ndenumerate(mat.real):
     #     ax.text(j, i, '{:0.5f}'.format(z), ha='center', va='center')
     plt.subplot(211)
-    plt.plot(times,z_track, label='rank %s' % rank)
+    plt.plot(times, z_track, label='rank %s' % rank)
 
     plt.subplot(212)
     plt.plot(times, x_track, label='rank %s' % rank)
 
 plt.subplot(211)
-plt.plot(times,expect[1], label='Lindblad',color='black',linestyle='--')
-plt.plot(times,expect_unaltered[1], label='Dissipation free',linestyle='-.',color='gray')
+plt.plot(times, expect[1], label='Lindblad', color='black', linestyle='--')
+plt.plot(times, expect_unaltered[1], label='Dissipation free', linestyle='-.', color='gray')
 # plt.xlabel('Time')
 plt.ylabel('$\\langle S_Z\\rangle$')
 
 plt.subplot(212)
-plt.plot(times,expect[0], label='Lindblad',color='black',linestyle='--')
-plt.plot(times,expect_unaltered[0], label='dissipation free',linestyle='-.',color='gray')
+plt.plot(times, expect[0], label='Lindblad', color='black', linestyle='--')
+plt.plot(times, expect_unaltered[0], label='dissipation free', linestyle='-.', color='gray')
 plt.xlabel('Time')
 plt.ylabel('$\\langle S_X\\rangle$')
 plt.legend()
 plt.show()
-
-
-
-
-
-
